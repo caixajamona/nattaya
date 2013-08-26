6ae337188126b3b864c36cbbf10e733fd2372490
Nattaya Dilokwanich <contact.nattaya@gmail.com>
2013-08-26 22:46:41 +0700

    post - create post (new-create)/view post list (index)/see specific post (show)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,30 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.all
+		@posts = Post.order(:created_at).reverse
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(create_params)
+
+		if @post.save
+		    redirect_to @post
+	    else
+	        render 'new'
+	    end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+  private 
+	def create_params
+	params.require(:post).permit(
+	  :title, :text
+	)
+	end
 end
